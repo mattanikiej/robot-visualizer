f4fc91a1949bdb7be702c775c7a5122d9f859335
Matt Anikiej <mattanikiej@gmail.com>
2023-11-15 09:49:36 -0600

    positions animate well

--- historyLoader.js
+++ historyLoader.js
@@ -1,11 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// import { scene, camera, renderer } from "./main"
 
 let voxels = [];
 let settings = [];
 let clips = [];
-let mixers = [];
 let actions = [];
 
 let scene = null;
@@ -86,108 +84,156 @@ function createAnimation(data) {
   // console.log(steps);
 
   // extract initial state
-  const init_state = steps[0].split(">>>")[1].split(";");
+  // const init_state = steps[0].split(">>>")[1].split(";");
 
-  // create and arrange voxels at initial step
-  for (let i = 0; i < init_state.length-1; i++) {
-    // extract voxel information
-    const voxel_info = init_state[i].split(",");
+  let times = [];
+  let positions_per_voxel = [];
+  let rotations_per_voxel = [];
 
-    // position
-    let vx = parseFloat(voxel_info[0]) * rescale;
-    let vy = parseFloat(voxel_info[1]) * rescale;
-    let vz = parseFloat(voxel_info[2]) * rescale;
+  for (let j = 0; j < steps.length-1; j++) {
 
-    // orientation
-    let vow = parseFloat(voxel_info[3]);
-    let vox = parseFloat(voxel_info[4]);
-    let voy = parseFloat(voxel_info[5]);
-    let voz = parseFloat(voxel_info[6]);
+    // add current step time to the times array
+    const t_str = steps[j].split(">>>")[0].split(":")[1];
+    const t_float = parseFloat(t_str);
+    times.push(t_float);
 
-    // corners
-    let vnnnx = parseFloat(voxel_info[7]) * rescale;
-    let vnnny = parseFloat(voxel_info[8]) * rescale;
-    let vnnnz = parseFloat(voxel_info[9]) * rescale;
+    let positions = [];
+    let rotations = [];
 
-    let vpppx = parseFloat(voxel_info[10]) * rescale;
-    let vpppy = parseFloat(voxel_info[11]) * rescale;
-    let vpppz = parseFloat(voxel_info[12]) * rescale;
+    // get current step information
+    const step = steps[j].split(">>>")[1].split(";");
 
-    // material id
-    let vmid = parseFloat(voxel_info[13]);
+    for (let i = 0; i < step.length-1; i++) {
+      // extract voxel information
+      const voxel_info = step[i].split(",");
 
-    // console.log([vx, vy, vz, vow, vox, voy, voz, vnnnx, vnnny, vnnnz, vpppx, vpppy, vpppz, vmid]);
+      // position
+      let vx = parseFloat(voxel_info[0]) * rescale;
+      let vy = parseFloat(voxel_info[1]) * rescale;
+      let vz = parseFloat(voxel_info[2]) * rescale;
+      positions.push(vx);
+      positions.push(vy);
+      positions.push(vz);
 
-    // create voxel
-    // set geo
-    const geometry = new THREE.BoxGeometry(
-      1 * voxel_size,
-      1 * voxel_size,
-      1 * voxel_size
-    );
+      // orientation
+      let vow = parseFloat(voxel_info[3]);
+      let vox = parseFloat(voxel_info[4]);
+      let voy = parseFloat(voxel_info[5]);
+      let voz = parseFloat(voxel_info[6]);
+      rotations.push(vox);
+      rotations.push(voy);
+      rotations.push(voz);
+      rotations.push(vow);
 
-    // set material
-    const material = new THREE.MeshBasicMaterial();
-    // material.wireframe = true;
+      // corners
+      let vnnnx = parseFloat(voxel_info[7]) * rescale;
+      let vnnny = parseFloat(voxel_info[8]) * rescale;
+      let vnnnz = parseFloat(voxel_info[9]) * rescale;
 
-    let r = mats[vmid - 1][0] * 100;
-    let g = mats[vmid - 1][1] * 100;
-    let b = mats[vmid - 1][2] * 100;
-    let rgb_str =
-      "rgb(" +
-      r.toString() +
-      "%, " +
-      g.toString() +
-      "%, " +
-      b.toString() +
-      "%)";
-    material.color = new THREE.Color(rgb_str);
+      let vpppx = parseFloat(voxel_info[10]) * rescale;
+      let vpppy = parseFloat(voxel_info[11]) * rescale;
+      let vpppz = parseFloat(voxel_info[12]) * rescale;
 
-    let a = mats[vmid - 1][3];
-    material.opacity = a;
+      // material id
+      let vmid = parseFloat(voxel_info[13]);
 
-    const cube = new THREE.Mesh(geometry, material);
+      // make a new cube only on initial step
+      if (j === 0) {
+        // create voxel
+        // set geo
+        const geometry = new THREE.BoxGeometry(
+          1 * voxel_size,
+          1 * voxel_size,
+          1 * voxel_size
+        );
 
-    // set voxel params
-    // position
-    cube.position.x = vx;
-    cube.position.y = vy;
-    cube.position.z = vz;
+        // set material
+        const material = new THREE.MeshBasicMaterial();
+        // material.wireframe = true;
 
-    // orientation
-    const quaternion = new THREE.Quaternion();
-    quaternion.setFromAxisAngle(new THREE.Vector3(vox, voy, voz), vow);
-    cube.setRotationFromQuaternion(quaternion);
+        let r = mats[vmid - 1][0] * 100;
+        let g = mats[vmid - 1][1] * 100;
+        let b = mats[vmid - 1][2] * 100;
+        let rgb_str =
+          "rgb(" +
+          r.toString() +
+          "%, " +
+          g.toString() +
+          "%, " +
+          b.toString() +
+          "%)";
+        material.color = new THREE.Color(rgb_str);
 
-    voxels.push(cube);
-    scene.add(cube);
+        let a = mats[vmid - 1][3];
+        material.opacity = a;
+
+        const cube = new THREE.Mesh(geometry, material);
+
+        // set voxel params
+        // position
+        cube.position.x = vx;
+        cube.position.y = vy;
+        cube.position.z = vz;
+
+        // orientation
+        const quaternion = new THREE.Quaternion();
+        quaternion.setFromAxisAngle(new THREE.Vector3(vox, voy, voz), vow);
+        cube.setRotationFromQuaternion(quaternion);
+
+        voxels.push(cube);
+        scene.add(cube);
+      }
+
+      positions_per_voxel.push(positions);
+      rotations_per_voxel.push(rotations);
+      
+    }
   }
 
   renderer.render(scene, camera);
-  kfAnimate();
+  kfAnimate(times, positions_per_voxel, rotations_per_voxel);
 }
 
-function kfAnimate() {
+function kfAnimate(times, positions_per_voxel, rotations_per_voxel) {
 
-  const times = [0, 3, 6];
-  const values = [0, 0, 0, 0.05, 0.05, 0.05, 0, 0, 0];
+  // const positionKF = new THREE.VectorKeyframeTrack(".position", times, positions);
+  // const rotationKF = new THREE.VectorKeyframeTrack(".quaternion", times, positions);
 
-  const positionKF = new THREE.VectorKeyframeTrack(".position", times, values);
+  // const tracks = [positionKF, rotationKF];
 
-  const tracks = [positionKF];
+  // const length = -1;
 
-  const length = -1;
-  
-  const clip = new THREE.AnimationClip('slowmove', length, tracks);
-  clips.push(clip);
+  // create animation for each voxel
+  for (let i = 0; i < voxels.length; i++) {
 
-  const mixer = new THREE.AnimationMixer(voxels[0]);
-  mixers.push(mixer);
+    let positions = [];
+    // get position key frames for the voxel
+    for (let p = 0; p < positions_per_voxel.length; p++) {
+      let vx = positions_per_voxel[p][i*3];
+      let vy = positions_per_voxel[p][i*3+1];
+      let vz = positions_per_voxel[p][i*3+2];
 
-  const action = mixer.clipAction(clip);
-  actions.push(action);
+      positions.push(vx);
+      positions.push(vy);
+      positions.push(vz);
+    }
 
-  action.play();
+    const positionKF = new THREE.VectorKeyframeTrack(".position", times, positions);
+
+    const tracks = [positionKF];
+    
+    const length = -1;
+
+    const clip = new THREE.AnimationClip('robot-simulation'+toString(i), length, tracks);
+    clips.push(clip);
+    
+    const mixer = new THREE.AnimationMixer(voxels[i]);
+    const action = mixer.clipAction(clip);
+
+    action.play();
+
+    actions.push(action);
+  }
 
   animate();
 
@@ -195,12 +241,15 @@ function kfAnimate() {
 
 function animate() {
   requestAnimationFrame(animate);
-
-  const mixer = actions[0].getMixer();
-
+  // get clock update
   const delta = clock.getDelta();
-  mixer.update(delta);
 
+  // update actions
+  for (let i = 0; i < actions.length; i++) {
+    const mixer = actions[i].getMixer();
+    mixer.update(delta);
+  }
+  
   // update controls
   controls.update();
 
